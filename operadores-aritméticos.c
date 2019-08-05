04b348044223d6bb257fe7178a192c7e1c9966e0
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-05 19:55:55 -0300

    2°-Operadores Aritméticos

---
+++ operadores-aritméticos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Operadores aritiméticos*/
+
+void main(void) {
+	printf("a soma e de %d\n",10+10);
+	printf("a subtracao e %d\n", 10-8);
+	printf("a multiplicacao e de %d\n",5*5);
+	printf("A divisao e de %d\n",10/2);
+	printf("o resultado da divisao e de %d\n",10%5);
+}
