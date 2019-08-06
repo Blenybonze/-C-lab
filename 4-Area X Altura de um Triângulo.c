9d038fa116dcfb8abbd4c7ce117b0a936236062a
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-05 22:23:36 -0300

    4°-Area X Altura de um Triângulo.c

--- 4-Area X Altura de um Triângulo.c
+++ 4-Area X Altura de um Triângulo.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Escreva um algoritmo para ler a base e a altura de um triangulo. E em seguida, calcule a área do mesmo. */
+/* Escreva um algoritmo para ler a base e a altura de um triangulo. E em seguida, calcule a Ã¡rea do mesmo. */
 
 int base,altura;
 float area;
-int main() {
-	
-	printf("Digite a base do triangulo: ");
-	scanf("%d",&base);
-	printf("Digite a altura do triangulo: ");
-	scanf("%d",&altura);
-	area=(base*altura)/2;
-	printf("A area do triangulo e de: %.f",area);
-	getch();
+
+int main()
+{
+    printf("Digite a base do triangulo: ");
+    scanf("%d",&base);
+    printf("Digite a altura do triangulo: ");
+    scanf("%d",&altura);
+    
+    area=(base*altura)/2;
+    
+    printf("A area do triangulo e de: %f",area);
+    
+    getch();
 	
 }
