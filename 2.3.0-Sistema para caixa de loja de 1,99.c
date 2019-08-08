1f6ab47e6547faac6655f557d497aa64ce9e70ce
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-08 20:43:46 -0300

    2.3.0-Sistema para caixa de loja de 1,99.c

--- 2.3.0-Sistema para caixa de loja de 1,99.c
+++ 2.3.0-Sistema para caixa de loja de 1,99.c
@@ -4,7 +4,17 @@
 /* O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca de 10 caixas. Para agilizar o cálculo de
 quanto cada cliente deve pagar ele desenvolveu um tabela que contém o número de itens que o cliente comprou e ao lado o
 valor da conta. Desta forma a atendente do caixa precisa apenas contar quantos itens o cliente está levando e olhar na 
-tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços. */
+tabela de preços. Você foi contratado para desenvolver o programa que monta esta tabela de preços. 
+
+Lojas Quase Dois - Tabela de preços
+1 - R$ 1.99
+2 - R$ 3.98
+...
+50 - R$ 99.50
+*/
+
+/*Edit: Aproveitei e fiz melhor do que pedia, em vez de repetir até 50 itens de 1.99 dei a opção do funcionário colocar a quantidade apenas
+e o programaja calcula o total, solicitando o valor dado pelo cliente e calculando o troco a ser devolvido.*/
 
 int i=0,reiniciar;
 float itens=0,preco=1.99,total,troco;
@@ -14,6 +24,7 @@ int main()
 
     do
     {
+	printf
         printf("Quantidade de Itens comprados: ");
         scanf("%f",&itens);
 
