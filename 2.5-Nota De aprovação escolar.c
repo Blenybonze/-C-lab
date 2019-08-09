fc7d83d15c2248c674c5b1034b13de6ed325a152
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-09 19:28:32 -0300

    2.5-Nota De aprovação escolar.c
    Crie um Programa que receba uma nota(entre 0.0 e 10,0) e checa se você passou direto,
    ficou de recuperação ou foi reprovado na matéria

--- 2.5-Nota De aprovação escolar.c
+++ 2.5-Nota De aprovação escolar.c
@@ -1,38 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Crie um Programa que receba uma nota(entre 0.0 e 10,0) e checa se você passou direto,
-ficou de recuperação ou foi reprovado na matéria: 
-
+/* Crie um Programa que receba uma nota(entre 0.0 e 10,0) e checa se vocÃª passou direto,
+ficou de recuperaÃ§Ã£o ou foi reprovado na matÃ©ria:
 SENDO:
-
 7 ou mais ------- Passou direto
-Entre 4 e 7 ----- Recuperação
+Entre 4 e 7 ----- RecuperaÃ§Ã£o
 Abaixo de 4 ----- Reprovado Direto
-
 */
-
 
 float n1;
 
-void main(void) {
-	
+void main(void)
+{
 	printf("Digite a sua nota: ");
 	scanf("%f",&n1);
-	
-	
-	if(n1>=7&&n1<=10){
-		printf("passou direto");
-	}
-	else if(n1>=4&&n1<7){
-		printf("Graças a Deus, ficou de recuperacao");
-	}
-	else{
-		printf("voce foi reprovado");
-	
-	}
-	
 
-	
-	
+
+	if(n1>=7&&n1<=10)
+	    printf("Passou direto");
+
+	else if(n1>=4&&n1<7)
+	    printf("Ficou de recuperacao");
+
+	else
+	    printf("Voce foi reprovado");
+
 }
