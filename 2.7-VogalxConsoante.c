301f1a2584d9ff9c3b2571d617f05b79fbc5cd3b
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-09 19:41:55 -0300

    2.7-VogalxConsoante.c

--- 2.7-VogalxConsoante.c
+++ 2.7-VogalxConsoante.c
@@ -2,31 +2,28 @@
 #include <stdlib.h>
 
 /*
-Crie um algoritmo que Identifique se a letra digitada é VOGAL ou CONSOANTE:
-Funções para caracteres:
-toupper = Maiúsculas
+Crie um algoritmo que Identifique se a letra digitada Ã© VOGAL ou CONSOANTE:
+FunÃ§Ãµes para caracteres:
+toupper = MaiÃºsculas
 tolower  = Minusculas
 */
 
 char letra;
-void main(void) {
-	printf("Digite uma letra: ");
-	scanf("%c",&letra);
-	switch(tolower(letra))//Tolower é usado para converter letras digitadas maiúsculas para minusculas.
+void main(void)
 {
+    printf("Digite uma letra: ");
+    scanf("%c",&letra);
+
+    switch(tolower(letra))//Tolower Ã© usado para converter letras digitadas maiÃºsculas para minusculas.
+    {
 	case 'a':
-//	case 'A':
 	case 'e':
-//	case 'E':
 	case 'i':
-//	case 'I':
 	case 'o':
-//	case 'O':
 	case 'u':
-//	case 'U':
-		printf("VOGAL");
-	break;
+            printf("VOGAL");
+	    break;
 	default:
-		printf("Consoante");
-}
+            printf("Consoante");
+    }
 }
