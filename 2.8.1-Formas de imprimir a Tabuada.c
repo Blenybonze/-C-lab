772ec6d03e1132e8937eb3f1d01edec33e223e53
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-09 19:49:50 -0300

    2.8.1-Formas de imprimir a Tabuada.c

---
+++ 2.8.1-Formas de imprimir a Tabuada.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* exercicio Do While:
+Fazer uma tabuada de multiplicação de 1 a 10 uma embaixo da outra, e após isso, modificar para ficar uma do lado da outra. 
+
+int main() {
+int n1=1,n2=1;
+
+do{
+    printf("%d x %d = %d   ",n1,n2,n1*n2);
+    n2=n2+1;
+if(n2>=11)
+{
+    printf("\n");
+	n2=1;
+    n1=n1+1;
+}
+else{
+}}
+while(n1<=10);
+
+	return 0;
+}
+*/
+
+
+/* Exercicio 2 utilizando FOR
+criar tabuada de 1 a 10 de multiplicação utilizando apenas FOR aninhado:
+
+int main(){
+int i,j;
+	
+	for(i=1;i<10;i++){
+    
+    for(j=1;j<=10;j++){
+    printf("%d x %d = %d\n",i,j,i*j);
+}   
+    printf("\n");
+}
+	
+
+    
+    
+    
+	return 0;
+}
+
+*/
+
