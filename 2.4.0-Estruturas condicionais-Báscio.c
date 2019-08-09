09c1a126e8905a33de409ccb04ff177bec48a382
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-09 19:20:44 -0300

    2.4.0-Estruturas condicionais-Báscio.c
    Faça um programa que peça dois números inteiros ao usuario e mostre qual o maior e qual o menor

--- 2.4.0-Estruturas condicionais-Báscio.c
+++ 2.4.0-Estruturas condicionais-Báscio.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Faça um programa que peça dois números inteiros ao usuario e mostre qual o maior e qual o menor.*/
+/* FaÃ§a um programa que peÃ§a dois nÃºmeros inteiros ao usuario e mostre qual o maior e qual o menor.*/
 
 int n1,n2;
-void main(void) {
+void main(void)
+{
 	printf("Digite o primeiro numero inteiro: ");
 	scanf("%d",&n1);
 	printf("Digite o segundo numero inteiro: ");
 	scanf("%d",&n2);
 
-	if(n1>n2){
+	if(n1>n2)
 		printf("O primeiro numero inteiro e maior: %d",n1);
-		
-	}else{
+
+	else
 		printf("O segundo numero inteiro e maior: %d",n2);
-    }
+
 }
-    
+
 	
 
 
