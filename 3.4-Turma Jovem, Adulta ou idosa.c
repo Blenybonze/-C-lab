f435677cabd64a7e39811bd9301caa988a4510a4
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-10 15:36:51 -0300

    3.4-Turma Jovem, Adulta ou idosa.c

--- 3.4-Turma Jovem, Adulta ou idosa.c
+++ 3.4-Turma Jovem, Adulta ou idosa.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* FaÁa um programa que peÁa para N pessoas a sua idade, ao final o programa devera verificar se
- a mÈdia de idade da turma varia entre 0 e 25,26 e 60 e maior que 60; e ent„o,
-dizer se a turma È jovem, adulta ou idosa, conforme a mÈdia calculada. */
+/* Fa√ßa um programa que pe√ßa para N pessoas a sua idade, ao final o programa devera verificar se
+ a m√©dia de idade da turma varia entre 0 e 25,26 e 60 e maior que 60; e ent√£o,
+dizer se a turma √© jovem, adulta ou idosa, conforme a m√©dia calculada. */
 
 int i,idade; //Idade digitada pelo usuario
 int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10; //Alunos
 float media,idadetotal; //variaveis responsaveis para armazenamento de numeros digitados
-int reset; //variavel responsavel por dar opÁ„o ao usuario de reiniciar o programa
+int reset; //variavel responsavel por dar op√ß√£o ao usuario de reiniciar o programa
 
 int main()
 {
-	for(i=1;i<=10;i++)
-	{
-		printf("Idade do Aluno: ");
-		scanf("%d",&idade);
+    for(i=1;i<=10;i++)//10 alunos
+    {
+	printf("Idade do Aluno: ");
+	scanf("%d",&idade);
 
-		idadetotal=idadetotal+idade;
+	idadetotal=idadetotal+idade;
 
-		if(i==10)
+	if(i==10)
         {
-            media=idadetotal/10;
+	    media=idadetotal/10;
             printf("\nMedia de idade da turma: %f\n",media);
 
-		    if(media>0&&media<=25)
-	        {
-	    	    printf("\nOs alunos desta turma sao Jovens.");
-	        }
+            if(media>0&&media<=25)
+	        printf("\nOs alunos desta turma sao Jovens.");
+
             else if(media>=26&&media<=60)
-            {
                 printf("\nOs alunos desta turma sao Adultos.");
-            }
+
             else if(media>60)
-            {
                 printf("\nOs alunos desta turma sao Idosos.");
-            }
+
 
             printf("\nDeseja tirar a media de uma nova turma?\n[1]-SIM   [2]-NAO\n");
             scanf("%d",&reset);
@@ -50,6 +47,6 @@ int main()
                     printf("Programa Encerrado.\n---------------------\n");
             }
         }
-	}
+    }
     return 0;
 }
