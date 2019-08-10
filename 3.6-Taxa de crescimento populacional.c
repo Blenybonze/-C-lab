cd135eb09e26b9d65171171be7c107040eb4b301
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-10 15:56:08 -0300

    3.6-Taxa de crescimento populacional.c

--- 3.6-Taxa de crescimento populacional.c
+++ 3.6-Taxa de crescimento populacional.c
@@ -3,35 +3,43 @@
 
 /* 
 Exercicio usando for.
-1 - Supondo que a populaÁ„o de um paÌs A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a populaÁ„o de B seja 200000 habitantes
-com uma taxa de crescimento de 1.5%. FaÁa um programa que calcule e escreva o 
-n˙mero de anos necess·rios para que a populaÁ„o do paÌs A ultrapasse ou iguale a populaÁ„o do paÌs B, mantidas as taxas de crescimento.
+1 - Supondo que a popula√ß√£o de um pa√≠s A seja da ordem de 80000 habitantes com uma taxa anual de crescimento de 3% e que a 
+popula√ß√£o de B seja 200000 habitantes com uma taxa de crescimento de 1.5%. Fa√ßa um programa que calcule e escreva o 
+n√∫mero de anos necess√°rios para que a popula√ß√£o do pa√≠s A ultrapasse ou iguale a popula√ß√£o do pa√≠s B, mantidas as taxas de crescimento.
 
-2- Altere o programa anterior permitindo ao usu·rio informar as populaÁıes e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operaÁ„o. */
+2- Altere o programa anterior permitindo ao usu√°rio informar as popula√ß√µes e as taxas de crescimento iniciais. Valide a entrada 
+e permita repetir a opera√ß√£o. */
 
-int main() {
 int i,anos=1;
 int A,B;
 float pa,pb;
-printf("Este programa foi criado para\nCalcular quanto tempo levaria para a cidade A alcancar o numero populacional da Cidade B\n----------------------------------------------\n");
-printf("Qual o numero populacional da Cidade A e quantos %(de 1 a 100) ela cresce ao ano: ");
-scanf("%d%f",&A,&pa);
-printf("Qual o numero populacional da Cidade B e quantos %(de 1 a 100) ela cresce ao ano: ");
-scanf("%d%f",&B,&pb);
 
-pa=pa/100;
-pb=pb/100;
+int main() 
+{
+    printf("Este programa foi criado para\nCalcular quanto tempo levaria para a cidade A alcancar o numero populacional da Cidade B\n----------------------------------------------\n");
+    printf("Qual o numero populacional da Cidade A e quantos %(de 1 a 100) ela cresce ao ano: ");
+    scanf("%d%f",&A,&pa);
+    printf("Qual o numero populacional da Cidade B e quantos %(de 1 a 100) ela cresce ao ano: ");
+    scanf("%d%f",&B,&pb);
 
-    for(i=0;i<=10;i++){
-    A=A+A*pa;
-    B=B+B*pb;
+    pa=pa/100;
+    pb=pb/100;
+
+    for(i=0;i<=10;i++)
+    {
+        A=A+A*pa;
+        B=B+B*pb;
 	anos=anos+1;	
-	if(A>=B){
-    printf("A quantidade de anos para Cidade A igualar a populaÁ„o da cidade B e:%d",anos);
-    i=10;
+	
+	if(A>=B)
+	{
+            printf("A quantidade de anos para Cidade A igualar a popula√ß√£o da cidade B e:%d",anos);
+            i=10;
+        }
+        else
+	{
+            printf(".\n");
+            i=0;
+        }
+    }
 }
-    else{
-    printf(".\n");
-    i=0;
-}
-}}
