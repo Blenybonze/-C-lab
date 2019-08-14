aa583766f461188ce8a06064f857f7155565ef2d
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-08-14 12:28:13 -0300

    4.0-comparação de clientes de academia

--- 4.0-comparação de clientes de academia.c
+++ 4.0-comparação de clientes de academia.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /*Uma academia deseja fazer um senso entre seus clientes para descobrir o mais alto, o mais baixo, a mais gordo e o mais magro,
- para isto você deve fazer um programa que pergunte a cada um dos clientes da academia seu código, sua altura e seu peso.
- O final da digitação de dados deve ser dada quando o usuário digitar 0 (zero) no campo código. Ao encerrar o programa também
- deve ser informados os códigos e valores do clente mais alto, do mais baixo, do mais gordo e do mais magro, além da média das
+ para isto vocÃª deve fazer um programa que pergunte a cada um dos clientes da academia seu cÃ³digo, sua altura e seu peso.
+ O final da digitaÃ§Ã£o de dados deve ser dada quando o usuÃ¡rio digitar 0 (zero) no campo cÃ³digo. Ao encerrar o programa tambÃ©m
+ deve ser informados os cÃ³digos e valores do clente mais alto, do mais baixo, do mais gordo e do mais magro, alÃ©m da mÃ©dia das
   alturas e dos pesos dos clientes
   */
 
@@ -19,7 +19,7 @@ int main()
         scanf("%d",&codigo);
         mt=i-1;
         kg=i-1;
-
+     
         if(codigo>0)
         {
             printf("Digite sua altura em metros: ");
